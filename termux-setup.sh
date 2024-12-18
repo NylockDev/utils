62eff2925bf865e337076ceb669d2278f3fd1298
Toussaint Adou <adoutoussaint5@gmail.com>
2024-12-18 15:54:52 +0000

    init

---
+++ termux-setup.sh
@@ -0,0 +1,55 @@
+apt install lolcat && lolcat 'toi Christ tu est un vieux pong'
+echo au suivant
+sleep 2
+apt install sl && sl
+echo
+figlet on continue 
+sleep 1
+apt install nyancat
+apt install git python -y
+apt install termux-api -y
+apt install sreenfetch -y
+apt install vim -y
+apt update
+apt install figlet && figlet nylock
+apt install rich -y && rich
+apt install cmatrix -y
+apt install lsd
+lsd
+apt install tmux -y
+apt install cowsay && cowsay Bonjour Christ le pong | lolcat
+apt install zsh
+neofetch
+cowsay c'est cool n'est ce pas
+
+echo nettoyons tous ca 
+clear
+apt install toilet 
+echo "╱╱╱╭━╮╱╱╱╱╭━╮╱╱╱
+╱╱╱┃╮┗━━━━┛╭┃╱╱╱
+╱╱╱╰╱┈╮┈┈╭┈╲╯╱╱╱
+╱╱╱▕▕▊┈┈┈▕▊┈▏╱╱╱
+╭┳╮┈╲┈┈▅┈┈┈╱┈╭┳╮
+┃╰╰▔▔╲╰┻╯┈╱▔▔╯╯┃
+╰━━━╮┈▔▔▔▔┈╭━━━╯
+╱╱╱╱┃┊┊┊┊┊┊┃╱╱╱╱
+╭━╮╱┃┈┈╭╮┈┈┃╱╭━╮
+╰┳╯╱┃┈┈┃┃┈┈┃╱╰┳╯
+┻┻┈╭╯┈┈┣┫┈┈╰╮┈┻╨
+╭╮▔╰┻┻━╯╰━┻┻╯▔┈┻
+"
+screenfetch -A 'Ubuntu'
+clear
+apt install asciinema
+
+echo '============================='
+echo ' NYLOCK DEV tes le meuilleur|'
+echo '============================|'
+echo 'bon cest fini' | lolcat
+cd $PREFIX/etc
+echo " echo 'bienvenu dans termux Tonton Christ 
+..__..
+{o,o}
+/)__) ---|lolcat'" >> termux-login.sh
+
+
